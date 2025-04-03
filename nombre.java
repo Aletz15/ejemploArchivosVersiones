121424e811213024b768b17213c93102cb85397c
Aletz15 <alejandrogoga15@gmail.com>
2025-04-02 22:45:43 -0600

    Impresion de Nombre

---
+++ nombre.java
@@ -0,0 +1,12 @@
+package propoo;
+
+public class nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Alejandro Efrain Gomez Garcia");
+
+	}
+
+}
